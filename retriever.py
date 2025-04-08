bbbd343c9edb4c1087e7d067b7affcfad8cf927c
Timostrijbis <49719336+Timostrijbis@users.noreply.github.com>
2025-04-08 15:15:57 +0200

    Cleaned up retriever.py, added additional comments

--- retriever.py
+++ retriever.py
@@ -9,37 +9,37 @@ def load_datasets(lang):
 
 def retriever(questions, corpus):
 
-    # co = cohere.Client("sr9NmsYnrQ2byCGENpTNy0m0mMyXgbcEJC7XXEYY")
-
     # Set number of retrieved passages
     top_k = 3
 
-    # Load at max 1000 chunks + embeddings
+    # If you're running a language with too much data, uncomment the next line and also lines 24 and 25
     max_docs = 5000000
+
+
     docs = []
     doc_embeddings = []
     for doc in corpus:
         docs.append(doc)
         doc_embeddings.append(doc['emb'])
-        if len(docs) >= max_docs:
-            break
+        # if len(docs) >= max_docs:
+        #     break
     doc_embeddings = np.asarray(doc_embeddings)
-    # embeddings = [x['emb'] for x in corpus]
-    # doc_embeddings = np.asarray(embeddings)
-
 
     output_list = {}
 
 
+    # Iterate through each question to get the embeddings
     for i in range(len(questions)):
         question_id = (questions[i]['id'])
         print(question_id)
         query = questions[i]['question']
         embedding = [questions[i]['emb']]
+        # Turn the embedding into a numpy array
         embedding = np.asarray(embedding)
 
         # Compute dot score between query embedding and document embeddings
         dot_scores = np.matmul(embedding, doc_embeddings.transpose())[0]
+        # Sort the dot scores in descending order, only select the top-k passages
         top_k_hits = np.argpartition(dot_scores, -top_k)[-top_k:].tolist()
 
         # Sort top_k_hits by dot score
@@ -47,9 +47,6 @@ def retriever(questions, corpus):
 
         results = []
         for doc_id in top_k_hits:
-            # print(docs[doc_id]['title'])
-            # print(docs[doc_id]['text'])
-            # print(docs[doc_id]['url'], "\n")
             results.append(docs[doc_id]['text'])
         output_list[question_id] = results
     return output_list
@@ -60,10 +57,10 @@ def main():
     # Set language
     lang = "ha"
     questions, corpus = load_datasets(lang)
-    print(corpus)
-
 
     output = retriever(questions, corpus)
+
+    # Save the output to a JSON file
     with open(lang+'.json', 'w') as f:
       json.dump(output, f, indent=4)
 
